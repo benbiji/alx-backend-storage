6b38df0ef87bd6b94878bda785dcd3f3ea8565c7
Benbiji <50benbiji@gmail.com>
2024-01-17 22:02:58 +0000

    All Task

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- List bands with Glam rock as their style, ordered by longevity
+
+SELECT band_name, IFNULL(split, 2022)-IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
